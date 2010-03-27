75fa941bfb439f53e1ad4000e19a6b8899d308ed
Esa-Matti Suuronen <esa-matti@suuronen.org>
2010-03-27 21:19:41 +0200

    New example application. Readme added.

--- example.py
+++ example.py
@@ -4,10 +4,36 @@ Created on Mar 23, 2010
 @author: epeli
 
 
-This an example subssh application.
-
+Simple application examples
 
 '''
+
+import subssh
+
+
+@subssh.expose_as("hello")
+def hello(user, name):
+    """
+    Says hello to you
+    
+    usage: $cmd <your name>
+    """
+    print "Hello %s!" % name
+
+
+
+@subssh.expose_as("uptime")
+def uptime(user):
+    """
+    Displays uptime of the system
+    
+    Example application for integrating uptime of the host system
+    
+    """
+    return subssh.call(("uptime"))
+
+    
+
 
 
 
--- uptime.py
+++ uptime.py
@@ -7,20 +7,3 @@ Created on Mar 5, 2010
 '''
 
 
-import subssh
-
-class config:
-    HELLO = "default hello"
-
-
-
-
-@subssh.expose_as("uptime")
-def uptime(user):
-    """Example application for integrating uptime of the host system"""
-    subssh.writeln(config.HELLO)
-    return subssh.call(("uptime"))
-
-    
-
-
